1fd7f697e413e7daca9aaff04469deb18825d947
johnwake <jwakeling@gmail.com>
2014-01-15 15:02:28 +0000

    added fizzbuzz example

---
+++ fizzbuzz/FizzBuzz.rb
@@ -0,0 +1,9 @@
+( 1..100 ).each do |number|
+    if number % 3 == 0 && number % 5 == 0
+    puts "#{ number } | fizzbuzz"
+    elsif number % 3 == 0
+    puts "#{ number } | fizz"
+    elsif number % 5 == 0
+    puts "#{ number } | buzz"
+ end
+end
